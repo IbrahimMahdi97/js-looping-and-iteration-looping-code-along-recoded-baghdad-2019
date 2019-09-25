7532e7fe6090b6ac33327a06f95aaf676c7962ca
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2019-09-25 14:08:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Code your solutions in this file
 const names = ['Lisa', 'Kaitlin', 'Jan'];
 
+<<<<<<< HEAD
 function writeCards(names, event) {
   let birthday =[];
   event = "surprise";
   for (let i= 0; i < names.length; i++) {
   birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+=======
+function writeCards(names) {
+  let birthday =[];
+  for (let i= 0; i < names.length; i++) {
+   let birthday[i] = 'Thank you, '+ names[i] +', for the wonderful birthday gift!';
+>>>>>>> 792e29ad76481700dececae430e34f1e10b1227a
   }
   return birthday;
 }
